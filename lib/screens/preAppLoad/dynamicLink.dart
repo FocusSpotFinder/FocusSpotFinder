b4d0c23f24b39b3f4912938aae3bc9edf324236c
Hiba-Alsayegh <439200659@student.ksu.edu.sa>
2022-03-26 01:38:27 +0300

    Sprint 4

--- lib/screens/preAppLoad/dynamicLink.dart
+++ lib/screens/preAppLoad/dynamicLink.dart
@@ -5,7 +5,6 @@ import 'package:shared_preferences/shared_preferences.dart';
 class DynamicLinkService {
 
   void handleBackGroundDynamicLinks() async {
-    ///To bring INTO FOREGROUND FROM DYNAMIC LINK.
     FirebaseDynamicLinks.instance.onLink.listen((initialLink) {
       print('back initialLink ${initialLink.link.path}');
       print(initialLink?.link);
@@ -18,28 +17,23 @@ class DynamicLinkService {
     });
 
   }
-  // bool _deeplink = true;
+
   void handleInitialDeepLink() async {
     final PendingDynamicLinkData initialLink = await FirebaseDynamicLinks.instance.
     getInitialLink();
-    // getDynamicLink(Uri.parse(link));
     print('main initialLink');
     print(initialLink?.link);
     if (initialLink != null) {
       handleDLink(initialLink);
     }
-    // Example of using the dynamic link to push the user to a different screen
-    // Navigator.pushNamed(context, deepLink.path);
   }
 
   buildDynamicLink(Uri link)async{
     String i='https://focusspotfinder.page.link';
     final DynamicLinkParameters parameters = DynamicLinkParameters(
-      // The Dynamic Link URI domain. You can view created URIs on your Firebase console
       uriPrefix: i,
-      // The deep Link passed to your application which you can use to affect change
       link: Uri.parse('https://focusspotfinder.com/story?id=$link'),
-      // Android application details needed for opening correct app on device/Play Store
+      // Android  details needed for opening correct app on device/Play Store
       androidParameters: const AndroidParameters(
         packageName: 'com.example.focus_spot_finder',
         minimumVersion: 1,
